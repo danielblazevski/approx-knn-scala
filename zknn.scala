418b43fc134b0c6ab24801fc6fdb109540411df9
danielblazevski <daniel.blazevski@gmail.com>
2016-02-16 23:59:03 -0500

    debugged issue, computes z values for shifted points, but computes knn for non-shifted points.  refactored a bit too

--- zknn.scala
+++ zknn.scala
@@ -1,107 +1,105 @@
-/////////// z-knn
+// z-knn
 import scala.collection.mutable.ListBuffer
 import scala.collection.SortedSet
 
-object zknn{
+object zknn {
 
-	val alpha = 1
-	val gamma = 1
-	val r = scala.util.Random
+  val alpha = 1
+  val gamma = 1
+  val r = scala.util.Random
 
-	def interleave(in:ListBuffer[String]): String = {
-		// get max length
-		val maxLen = in.map(str => str.length).max
-		val L = in.length
-		var res = ""
+  type Point = ListBuffer[Double]
 
-		for (i <- 0 until maxLen){
-			for (j <- 0 until L){
-				if(i >= in(L - 1 - j).length){
-					res = 0 + res
-				}else{
-					res = in(L - 1 - j)(in(L - 1 - j).length - 1 - i) + res
-				}
-			}
-		}
-		res
-	}
+  def interleave(in: ListBuffer[String]): String = {
+    // get max length
+    val maxLen = in.map(str => str.length).max
+    val L = in.length
+    var res = ""
 
-	def zValue(in:ListBuffer[Double]): Int = { //made un-scalaesque to debug
-		val interl = interleave(in.map(x=>x.toInt.toBinaryString))
-		val res = Integer.parseInt(interl,2)
-		res
-	}
+    for (i <- 0 until maxLen) {
+      for (j <- 0 until L) {
+        if (i >= in(L - 1 - j).length) {
+          res = 0 + res
+        } else {
+          res = in(L - 1 - j)(in(L - 1 - j).length - 1 - i) + res
+        }
+      }
+    }
+    res
+  }
 
- 	def subTwo(tuple: (ListBuffer[Double], Double)) = tuple._2
+  def zValue(in: Point): Int = {
+    Integer.parseInt(interleave(in.map(x => x.toInt.toBinaryString)), 2)
+  }
 
- 	def distance(a:ListBuffer[Double], b:ListBuffer[Double]): Double = {
- 		math.sqrt(a.zipWithIndex.map{ x => 
- 									(x._1 - b(x._2))*(x._1 - b(x._2))
- 									}.sum)
- 	}
+  def subTwo(tuple: (ListBuffer[Double], Double)) = tuple._2
 
-	def basicknnQuery(train:ListBuffer[ListBuffer[Double]], test:ListBuffer[ListBuffer[Double]], k: Int):
-	ListBuffer[(ListBuffer[Double], Array[ListBuffer[Double]])] = {
-		test.map{ v => (v,
-			train.map{
-      		x => (x, distance(v, x))
-      		}.sortBy(_._2).take(k).map(_._1).toArray)
-				}
-	}
+  def distance(a: Point, b: Point): Double = {
+    math.sqrt(a.zipWithIndex.map { x =>
+      (x._1 - b(x._2)) * (x._1 - b(x._2))
+    }.sum)
+  }
 
-/////////////// main zknn query
-	def zknnQuery(train:ListBuffer[ListBuffer[Double]], test:ListBuffer[ListBuffer[Double]], k: Int):
-	ListBuffer[(ListBuffer[Double], Array[ListBuffer[Double]])] = {
+  def basicknnQuery(train: ListBuffer[Point], test: ListBuffer[Point], k: Int):
+  ListBuffer[(ListBuffer[Double], Array[ListBuffer[Double]])] = {
+    test.map { v => (v,
+      train.map {
+        x => (x, distance(v, x))
+      }.sortBy(_._2).take(k).map(_._1).toArray)
+    }
+  }
 
-		var candidatePointsFromZvalue = new ListBuffer[ListBuffer[Double]]
-		val rSeq = Seq.fill(alpha)(Seq.fill(train.head.length)(r.nextFloat))
+  // main zknn query
+  def zknnQuery(train: Seq[Point], test: Seq[Point], k: Int):
+  ListBuffer[(Point, Array[Point])] = {
 
-		var res = new ListBuffer[(ListBuffer[Double], Array[ListBuffer[Double]])]
-		val v = test.head
-		//for (v <- test){
-			for (i <- 0 until alpha){
-				val zQueryShifted = zValue(v.zipWithIndex.map{vZip => vZip._1 - rSeq(i)(vZip._2)})
-				val zTrainSetShiftedSortedPre = train.map{ trainPoint => 
-						trainPoint.zipWithIndex.map(trainPointZipped => trainPointZipped._1 - rSeq(i)(trainPointZipped._2))
-						}
-				
-				val zTrainSetShiftedSorted = zTrainSetShiftedSortedPre.map{
-							shiftTrainPoint => 
-							(shiftTrainPoint, zValue(shiftTrainPoint)) 
-							}//.sortBy(x => x._2)
+    var candidatePointsFromZvalue = new ListBuffer[Point]
+    val rSeq = Seq.fill(alpha)(Seq.fill(train.head.length)(r.nextFloat))
 
-				// get 2*gamma points about query point q, need to check if near end...wip
-				val zTrainSetShiftedByZTrain = zTrainSetShiftedSorted.map
-											{ tuple => 
-											(tuple._1 , tuple._2 - zQueryShifted)
-											}
+    var res = new ListBuffer[(Point, Array[Point])]
+    val v = test.head
+    for (i <- 0 until alpha) {
+      val zQueryShifted = zValue(v.zipWithIndex.map { vZip => vZip._1 - rSeq(i)(vZip._2) })
 
-				val posFilter = zTrainSetShiftedByZTrain.filter(x => x._2 > 0)
-				val negFilter = zTrainSetShiftedByZTrain.filter(x => x._2 < 0)
+      val zTrainSetShiftedSorted = train.map { trainPoint => (trainPoint,
+        trainPoint.zipWithIndex.map(trainPointZipped =>
+          trainPointZipped._1 - rSeq(i)(trainPointZipped._2)))
+      }.map {
+        shiftTrainPoint =>
+          (shiftTrainPoint._1, zValue(shiftTrainPoint._2))
+      } .sortBy(x => x._2)
 
+      // get 2*gamma points about query point q, need to check if near end...wip
+      val zTrainSetShiftedByZTrain = zTrainSetShiftedSorted.map { tuple =>
+        (tuple._1, tuple._2 - zQueryShifted)
+      }
 
-				if (posFilter.length >= gamma && negFilter.length >= gamma){
-					println("posFilter.take(gamma).map(x => x._1)   =  " + posFilter.take(gamma).map(x => x._1))
-					println("negFilter.take(gamma).map(x => x._1)   =  " + negFilter.take(gamma).map(x => x._1))
-					candidatePointsFromZvalue = candidatePointsFromZvalue ++ posFilter.take(gamma).map(x => x._1)
-					candidatePointsFromZvalue = candidatePointsFromZvalue ++ negFilter.take(gamma).map(x => x._1)
+      val posFilter = zTrainSetShiftedByZTrain.filter(x => x._2 > 0)
+      val negFilter = zTrainSetShiftedByZTrain.filter(x => x._2 < 0)
 
-				}
-				res = res ++ basicknnQuery(candidatePointsFromZvalue, ListBuffer(v), k)
-			}
-			return res 
-	}
+      if (posFilter.length >= gamma && negFilter.length >= gamma) {
+        println("posFilter.take(gamma).map(x => x._1)   =  " + posFilter.take(gamma).map(x => x._1))
+        println("negFilter.take(gamma).map(x => x._1)   =  " + negFilter.take(gamma).map(x => x._1))
+        candidatePointsFromZvalue = candidatePointsFromZvalue ++ posFilter.take(gamma).map(x => x._1)
+        candidatePointsFromZvalue = candidatePointsFromZvalue ++ negFilter.take(gamma).map(x => x._1)
 
-	def main(args: Array[String]){
-		val lb = ListBuffer(12.2, 34.3, 2.0)
-		val Zval = zValue(lb)
-		println("zVal =  " + Zval)
+      }
+      res = res ++ basicknnQuery(candidatePointsFromZvalue, ListBuffer(v), k)
+    }
+    return res
+  }
 
-		val train = ListBuffer(ListBuffer(1.2, 4.3), ListBuffer(2.0, 0.0), ListBuffer(2.0, 1.0))
-		val test = ListBuffer(ListBuffer(0.8, 3.5))
+  def main(args: Array[String]) {
 
-		val knn = zknnQuery(train, test, 1)
-		println("nearest neighbor =  " + knn.head._2.head)
+    val lb = ListBuffer(12.2, 34.3, 2.0)
+    val Zval = zValue(lb)
+    println("zVal =  " + Zval)
 
-	}
+    val train = Seq(ListBuffer(1.2, 4.3), ListBuffer(2.0, 0.0), ListBuffer(2.0, 1.0))
+    val test = Seq(ListBuffer(0.8, 3.5))
+
+    val knn = zknnQuery(train, test, 1)
+    println("nearest neighborr =  " + knn.head._2.head)
+
+  }
 }
