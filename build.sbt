fe42a29c21a55e394be02aa422189d74285b3cac
danielblazevski <daniel.blazevski@gmail.com>
2016-02-17 12:30:39 -0500

    added single point version of basic knn and added capability for multiple test poitns, need to debug

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "z-knn"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
--- zknn.scala
+++ zknn.scala
@@ -49,6 +49,14 @@ object zknn {
     }
   }
 
+  def basicknnQuerySingleTest(train: ListBuffer[Point], test: Point, k: Int):
+  (Point, Array[Point]) = {
+    (test,
+      train.map {
+        x => (x, distance(test, x))
+      }.sortBy(_._2).take(k).map(_._1).toArray)
+    }
+
   // main zknn query
   def zknnQuery(train: Seq[Point], test: Seq[Point], k: Int):
   ListBuffer[(Point, Array[Point])] = {
@@ -57,34 +65,35 @@ object zknn {
     val rSeq = Seq.fill(alpha)(Seq.fill(train.head.length)(r.nextFloat))
 
     var res = new ListBuffer[(Point, Array[Point])]
-    val v = test.head
-    for (i <- 0 until alpha) {
-      val zQueryShifted = zValue(v.zipWithIndex.map { vZip => vZip._1 - rSeq(i)(vZip._2) })
+    for (v <- test) {
+      for (i <- 0 until alpha) {
+        val zQueryShifted = zValue(v.zipWithIndex.map { vZip => vZip._1 - rSeq(i)(vZip._2) })
 
-      val zTrainSetShiftedSorted = train.map { trainPoint => (trainPoint,
-        trainPoint.zipWithIndex.map(trainPointZipped =>
-          trainPointZipped._1 - rSeq(i)(trainPointZipped._2)))
-      }.map {
-        shiftTrainPoint =>
-          (shiftTrainPoint._1, zValue(shiftTrainPoint._2))
-      } .sortBy(x => x._2)
+        val zTrainSetShiftedSorted = train.map { trainPoint => (trainPoint,
+          trainPoint.zipWithIndex.map(trainPointZipped =>
+            trainPointZipped._1 - rSeq(i)(trainPointZipped._2)))
+        }.map {
+          shiftTrainPoint =>
+            (shiftTrainPoint._1, zValue(shiftTrainPoint._2))
+        }.sortBy(x => x._2)
 
-      // get 2*gamma points about query point q, need to check if near end...wip
-      val zTrainSetShiftedByZTrain = zTrainSetShiftedSorted.map { tuple =>
-        (tuple._1, tuple._2 - zQueryShifted)
+        // get 2*gamma points about query point q, need to check if near end...wip
+        val zTrainSetShiftedByZTrain = zTrainSetShiftedSorted.map { tuple =>
+          (tuple._1, tuple._2 - zQueryShifted)
+        }
+
+        val posFilter = zTrainSetShiftedByZTrain.filter(x => x._2 > 0)
+        val negFilter = zTrainSetShiftedByZTrain.filter(x => x._2 < 0)
+
+        if (posFilter.length >= gamma && negFilter.length >= gamma) {
+          println("posFilter.take(gamma).map(x => x._1)   =  " + posFilter.take(gamma).map(x => x._1))
+          println("negFilter.take(gamma).map(x => x._1)   =  " + negFilter.take(gamma).map(x => x._1))
+          candidatePointsFromZvalue = candidatePointsFromZvalue ++ posFilter.take(gamma).map(x => x._1)
+          candidatePointsFromZvalue = candidatePointsFromZvalue ++ negFilter.take(gamma).map(x => x._1)
+
+        }
+        res += basicknnQuerySingleTest(candidatePointsFromZvalue, v, k)
       }
-
-      val posFilter = zTrainSetShiftedByZTrain.filter(x => x._2 > 0)
-      val negFilter = zTrainSetShiftedByZTrain.filter(x => x._2 < 0)
-
-      if (posFilter.length >= gamma && negFilter.length >= gamma) {
-        println("posFilter.take(gamma).map(x => x._1)   =  " + posFilter.take(gamma).map(x => x._1))
-        println("negFilter.take(gamma).map(x => x._1)   =  " + negFilter.take(gamma).map(x => x._1))
-        candidatePointsFromZvalue = candidatePointsFromZvalue ++ posFilter.take(gamma).map(x => x._1)
-        candidatePointsFromZvalue = candidatePointsFromZvalue ++ negFilter.take(gamma).map(x => x._1)
-
-      }
-      res = res ++ basicknnQuery(candidatePointsFromZvalue, ListBuffer(v), k)
     }
      res
   }
@@ -96,10 +105,10 @@ object zknn {
     println("zVal =  " + Zval)
 
     val train = Seq(ListBuffer(1.2, 4.3), ListBuffer(2.0, 0.0), ListBuffer(2.0, 1.0))
-    val test = Seq(ListBuffer(0.8, 3.5))
+    val test = Seq(ListBuffer(0.8, 3.5), ListBuffer(1.9, 0.2))
 
     val knn = zknnQuery(train, test, 1)
-    println("nearest neighborr =  " + knn.head._2.head)
+    println("nearest neighborr =  " + knn)
 
   }
 }
